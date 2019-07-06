f357f2b204008fc2aaa2bce0729a7e7a4ff05a3e
Leandro Marinho <leandro.marinho.web@gmail.com>
2019-07-05 22:28:57 -0300

    Atualização 05/07/2019

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,11 +3,18 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
@@ -23,8 +30,11 @@ public class Sessao {
 	private Filme filme;
 
 	private LocalTime horario;
-	
+
 	private BigDecimal preco = BigDecimal.ZERO;
+
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
 
 	/**
 	 * @deprecated hibernate only
@@ -32,7 +42,7 @@ public class Sessao {
 	public Sessao() {
 
 	}
-	
+
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.setHorario(horario);
 		this.setFilme(filme);
@@ -71,13 +81,21 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
+
 	public BigDecimal getPreco() {
 		return preco.setScale(2, RoundingMode.HALF_UP);
 	}
 
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
+	}
+
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
+	}
+
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
 	}
 
 }
